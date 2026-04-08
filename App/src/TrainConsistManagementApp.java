ed0260a5a5f7add39b75cbf2118463d1d6df6488
devPotatoAni <hs363694@gmail.com>
2026-04-08 15:26:11 +0530

    Animesh added UC2

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,15 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class UC1InitializeTrainandDisplay {
+public class UC2AddPassengerBogies {
 
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
 
-        List<String> trainConsist = new ArrayList<>();
+        List<String> passengerBogies = new ArrayList<>();
 
-        System.out.println("Train consist initialized.");
-        System.out.println("Initial number of bogies: " + trainConsist.size());
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("Passenger bogies after addition:");
+        System.out.println(passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("After removing AC Chair:");
+        System.out.println(passengerBogies);
+
+        boolean exists = passengerBogies.contains("Sleeper");
+
+        System.out.println("Does Sleeper exist? " + exists);
+
+        System.out.println("Final bogie list:");
+        System.out.println(passengerBogies);
     }
 }
